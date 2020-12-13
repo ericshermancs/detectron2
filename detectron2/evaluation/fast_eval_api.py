c99f2d3c60085066bc45e9dd2ebd5eff7000f2d4
Eric Sherman <zensap92@gmail.com>
2020-12-12 21:40:09 -0500

    Update fast_eval_api.py

--- detectron2/evaluation/fast_eval_api.py
+++ detectron2/evaluation/fast_eval_api.py
@@ -13,6 +13,46 @@ class COCOeval_opt(COCOeval):
     and accumulate() are implemented in C++ to speedup evaluation
     """
 
+    def new_prepare(self):
+      '''
+        Prepare ._gts and ._dts for evaluation based on params
+        :return: None
+        '''
+        def _toMask(anns, coco):
+            # modify ann['segmentation'] by reference
+            for ann in anns:
+                if type(ann) ==list:
+                    ann = np.array(ann, dtype=2)
+                rle = coco.annToRLE(ann)
+                ann['segmentation'] = rle
+        p = self.params
+        if p.useCats:
+            gts=self.cocoGt.loadAnns(self.cocoGt.getAnnIds(imgIds=p.imgIds, catIds=p.catIds))
+            dts=self.cocoDt.loadAnns(self.cocoDt.getAnnIds(imgIds=p.imgIds, catIds=p.catIds))
+        else:
+            gts=self.cocoGt.loadAnns(self.cocoGt.getAnnIds(imgIds=p.imgIds))
+            dts=self.cocoDt.loadAnns(self.cocoDt.getAnnIds(imgIds=p.imgIds))
+
+        # convert ground truth to mask if iouType == 'segm'
+        if p.iouType == 'segm':
+            _toMask(gts, self.cocoGt)
+            _toMask(dts, self.cocoDt)
+        # set ignore flag
+        for gt in gts:
+            gt['ignore'] = gt['ignore'] if 'ignore' in gt else 0
+            gt['ignore'] = 'iscrowd' in gt and gt['iscrowd']
+            if p.iouType == 'keypoints':
+                gt['ignore'] = (gt['num_keypoints'] == 0) or gt['ignore']
+        self._gts = defaultdict(list)       # gt for evaluation
+        self._dts = defaultdict(list)       # dt for evaluation
+        for gt in gts:
+            self._gts[gt['image_id'], gt['category_id']].append(gt)
+        for dt in dts:
+            self._dts[dt['image_id'], dt['category_id']].append(dt)
+        self.evalImgs = defaultdict(list)   # per-image per-category evaluation results
+        self.eval     = {}                  # accumulated evaluation results
+
+
     def evaluate(self):
         """
         Run per image evaluation on given images and store results in self.evalImgs_cpp, a
@@ -36,7 +76,7 @@ class COCOeval_opt(COCOeval):
         p.maxDets = sorted(p.maxDets)
         self.params = p
 
-        self._prepare()
+        self.new_prepare()
 
         # loop through images, area range, max detection number
         catIds = p.catIds if p.useCats else [-1]
